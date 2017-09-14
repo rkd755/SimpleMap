936b99604f87fe3fdb182036d01f203b25f7d623
D7703_12 <d7703_12@D7703-12ui-iMac.local>
2017-09-14 15:57:36 +0900

    09.14 update

--- DITMap/ViewController.swift
+++ DITMap/ViewController.swift
@@ -18,9 +18,7 @@ class ViewController: UIViewController, MKMapViewDelegate {
         
         // DIT 위치정보 35.166197, 129.072594
         let center = CLLocationCoordinate2DMake(35.166197, 129.072594)
-        // DIT 반경 델타는 반경
         let span = MKCoordinateSpanMake(0.05, 0.05)
-        // 위치잡는거 center = 중앙 span = 확대
         let region = MKCoordinateRegionMake(center, span)
         
         myMapView.setRegion(region, animated: true)
@@ -28,26 +26,18 @@ class ViewController: UIViewController, MKMapViewDelegate {
         // Annotation(Pin) 꼽기
         let anno01 = MKPointAnnotation()
         anno01.coordinate = center
-        anno01.title = "부산시민공원"
-        anno01.subtitle = "단데기가 진화한 포켓몬"
+        anno01.title = "동의과학대"
+        anno01.subtitle = "편안하다"
         
-        myMapView.addAnnotation(anno01)
-        
-        // DIT 위치정보 35.168406, 129.057770
-        let center1 = CLLocationCoordinate2DMake(35.168406, 129.057770)
-        // DIT 반경 델타는 반경
-        let span1 = MKCoordinateSpanMake(0.05, 0.05)
-        // 위치잡는거 center = 중앙 span = 확대
-        let region1 = MKCoordinateRegionMake(center1, span1)
-        
-        myMapView.setRegion(region1, animated: true)
         
         // Annotation(Pin) 꼽기
         let anno02 = MKPointAnnotation()
-        anno02.coordinate = center1
+        anno02.coordinate.latitude = 35.168444
+        anno02.coordinate.longitude = 129.057832
         anno02.title = "부산시민공원"
-        anno02.subtitle = "건들면 안되는 포켓몬"
+        anno02.subtitle = "힘들다"
         
+        myMapView.addAnnotation(anno01)
         myMapView.addAnnotation(anno02)
         
         myMapView.delegate = self
@@ -68,14 +58,16 @@ class ViewController: UIViewController, MKMapViewDelegate {
             
             if annotation.title! == "부산시민공원" {
                 annotationView?.pinTintColor = UIColor.green
+            } else if annotation.title! == "동의과학대"{
+                annotationView?.pinTintColor = UIColor.blue
             }
             
             let leftIconView = UIImageView(frame: CGRect(x: 0, y: 0, width: 53, height: 53))
-            if annotation.title! == "부산시민" {
-            
+            if annotation.title! == "부산시민공원" {
             
             leftIconView.image = UIImage(named:"abc.jpg")
-            }else{    leftIconView.image = UIImage(named:"abc.jpg")
+            }else if annotation.title! == "동의과학대" {
+                leftIconView.image = UIImage(named:"abab.jpg")
             }
             annotationView?.leftCalloutAccessoryView = leftIconView
             
@@ -89,20 +81,33 @@ class ViewController: UIViewController, MKMapViewDelegate {
 
     public func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl){
         
-        print("Accessary Button tapped!!!!")
         
-        let viewAnno = view.annotation
-        let viewTitle: String = ((viewAnno?.title)!)!
-        let viewSubTitle: String = ((viewAnno?.subtitle)!)!
         
-        print("\(viewTitle) \(viewSubTitle)")
+        if control == view.rightCalloutAccessoryView{
+            
+            self.performSegue(withIdentifier: "goDetail", sender: self)
+        }
         
-        let ac = UIAlertController(title: viewTitle, message: viewSubTitle, preferredStyle: .actionSheet)
         
-        ac.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-        ac.addAction(UIAlertAction(title: "DELETE", style: .destructive, handler: nil))
+//------------------------------------------------------
+//        print("Accessary Button tapped!!!!")
+//        
+//        let viewAnno = view.annotation
+//        let viewTitle: String = ((viewAnno?.title)!)!
+//        let viewSubTitle: String = ((viewAnno?.subtitle)!)!
+//        
+//        print("\(viewTitle) \(viewSubTitle)")
+//        
+//
+//        let ac = UIAlertController(title: viewTitle, message: viewSubTitle, preferredStyle: .actionSheet)
+//        
+//        ac.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+//        ac.addAction(UIAlertAction(title: "DELETE", style: .destructive, handler: nil))
+//        
+//        present(ac, animated: true, completion: nil)
+//------------------------------------------------------
         
-        present(ac, animated: true, completion: nil)
+        
         
     }
 }
